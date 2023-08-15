21e4aa0611405edad0ed1e57a7de57dc481f01d6
admin <Ivan-nikolaenko@bk.ru>
2023-08-15 20:04:49 +0600

    Add new lessons

---
+++ step_2_core/lesson_15.py
@@ -0,0 +1,24 @@
+# students = ['Alex', 'Ivan', 'Jojo', 'Rita', 'Mojno']
+#
+# for f in students:
+#     var = 'Инженер '+f
+#     print(var)
+#
+# for i in range(100,1000):
+#     if i % 2 == 0 and i % 7 ==0:
+#         print(i)
+# # от 1 до 10
+# for i in range(1, 11):
+#     print(i)
+# посчитать сумму чисел от 1 до 100 включительно
+# total = 0
+# for i in range(1,101):
+#     total+=i
+# print(total)
+# вывести все нечетные числа до n
+# for i in range(1,101):
+#     if i % 3 ==0:
+#         print(i)
+
+r = list(range(4,11,2))
+print(r)
